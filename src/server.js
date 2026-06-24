d41f83f8083657feed9dd5d6ba6317d287558c2e
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-06-24 09:08:29 +0500

    feat(database): i add the database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
 import app from "./app.js";
+import connectDB from "./database/connection.js";
+import dotenv from "dotenv";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log("Server is Running Now ");
-});
+dotenv.config();
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(` Server Running on Port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+startServer();
